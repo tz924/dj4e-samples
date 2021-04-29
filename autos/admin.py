5643868a08d4ba7d3da09bb5049872a0c3e9975f
Thomas Zhang <zhj930924@gmail.com>
2021-04-29 02:12:07 -0700

    spacing

--- autos/admin.py
+++ autos/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from autos.models import Make, Auto
+from autos.models import Auto, Make
 
 # Register your models here.
 
-admin.site.register(Make)
 admin.site.register(Auto)
+admin.site.register(Make)
